8161746999539142b88a5cc3d07e7e9140067424
Parinay Rana <parinayrana@gmail.com>
2025-06-19 22:00:13 +0530

    OpenAi

---
+++ chatbot/app.py
@@ -0,0 +1,38 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+## environments variable call
+
+os.environ["OPENAI_API_KEY"]=   os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_API_KEY"]= os.getenv("LANGCHAIN_API_KEY")
+
+##creating chatbot 
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful mentor cum assistant. Please provde responses in a philosphical practical way taking references from the stoic writings"),
+        ("user", "Question:{question}")
+    ]
+)
+
+#streamlit framework
+
+st.title("Langchain Demo with OpenAI API")
+input_text = st.text_input("Search the things you want assistance in")
+
+#open ai llm call 
+llm = ChatOpenAI(model='gpt-3.5-turbo-0125')
+output_parser = StrOutputParser()
+
+# chain
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
