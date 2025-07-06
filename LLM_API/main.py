574b44f1928f9f6a384eb98e3e03ed2b75e6b097
Parinay Rana <parinayrana@gmail.com>
2025-07-06 22:25:36 +0530

    third commit

--- LLM_API/main.py
+++ LLM_API/main.py
@@ -2,3 +2,8 @@ from fastapi import FastAPI
 import ollama
 
 app = FastAPI()
+
+@app.post("/generate")
+def generate(prompt: str):
+    reponse = ollama.chat(model='mistral', messages = [{'role':"user", "content": prompt}])
+    return {"response": reponse['messages']['content']}
